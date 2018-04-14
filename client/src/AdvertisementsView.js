f2aad4fe2b185e1da04e901f4d2d1ddc0773e90d
Pavlo M <pavlomorozov78@gmail.com>
2018-04-14 09:12:47 +0300

    Fixed layout of AdvertisementsView date inputs

--- client/src/AdvertisementsView.js
+++ client/src/AdvertisementsView.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Col} from 'reactstrap';
 
 export default class AdvertisementsView extends Component {
   render() {
     return (
       <div>
         <h2> Advertisements view </h2>
-        <Form inline>
-          <FormGroup>
-            <Label for="openedDateFrom">Opened date from:</Label>
-            <Input type="date" name="date" id="openedDateFrom" placeholder="date placeholder" />
+        <Form>
+          <FormGroup row>
+            <Label for="openedDateFrom" md={4}>Opened from:</Label>
+            <Col md={4}>
+              <Input type="date" name="date" id="openedDateFrom" placeholder="date placeholder" />
+            </Col>
           </FormGroup>
-           <Button>Get advertisements</Button>
+          <FormGroup row>
+            <Label for="openedDateTo" md={4}>Opened to:</Label>
+            <Col md={4}>
+              <Input type="date" name="date" id="openedDateFrom" placeholder="date placeholder" />
+            </Col>
+          </FormGroup>
+
+          <Button>Get advertisements</Button>
+
         </Form>
       </div>
     );
