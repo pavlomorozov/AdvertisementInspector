79d5a322615c5127a09c8282de2061b0e061d8ae
Pavlo M <pavlomorozov78@gmail.com>
2018-06-06 16:36:32 +0300

    new Database class added to store database config and query mySQL pool. Database object queries return a Promise object and can be chained with .then() method

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require("mysql");
+var Database = require("./Database");
 
 var indexRouter = require('./routes/index');
 var lastAdvertisementsRouter = require('./routes/last-advertisements');
 var keyStatisticsRouter = require('./routes/key-statistics');
 var advertisementsViewTableRouter = require('./routes/advertisements-view-table');
+var advertisementDetailsRouter = require('./routes/advertisement-details');
 
 var app = express();
 
@@ -22,45 +23,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var dbConfig = {
-	connectionLimit : 10,
-	host     : '192.168.100.10',
-	user     : 'olx_user',
-	password : 'ghRd2#ju',
-	database : 'olxdb',
-	typeCast: function castField( field, useDefaultTypeCasting ) {
-		// We only want to cast bit fields that have a single-bit in them. If the field
-		// has more than one bit, then we cannot assume it is supposed to be a Boolean.
-		if ((field.type === "BIT") && (field.length === 1)) {
-			var bytes = field.buffer();
-			// A Buffer in Node represents a collection of 8-bit unsigned integers.
-			// Therefore, our single "bit field" comes back as the bits '0000 0001',
-			// which is equivalent to the number 1.
-			return (bytes[0] === 1);
-		}
-		return( useDefaultTypeCasting() );
-	}
-};
-
 app.use(function(req, res, next){
-	res.locals.mySQLPool = mysql.createPool(dbConfig);
-
-/*
-	res.locals.connection.connect(error => {
-		if (err){
-			console.log('Connection is asleep (time to wake it up): ', err);
-			res.locals.handleMySQLDisconnect();
-		}
-	});
-	*/
+	res.locals.mySQLDatabase = new Database();
 	next();
 });
-
 
 app.use('/', indexRouter);
 app.use('/api/last-advertisements', lastAdvertisementsRouter);
 app.use('/api/key-statistics', keyStatisticsRouter);
 app.use('/api/advertisements-view-table', advertisementsViewTableRouter);
+app.use('/api/advertisement-details', advertisementDetailsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
