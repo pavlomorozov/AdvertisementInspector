374d5808f8f1e6ba2798d15acf5911e3bc3ef451
Pavlo M <pavlomorozov78@gmail.com>
2018-06-06 16:38:57 +0300

    new class AdvertisementDetails makes 3 calls to databse, chains them with .then() and responses once all 3 queries data available

---
+++ routes/advertisement-details.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+	const detailsQuery = `
+    SELECT * FROM parameter
+    WHERE advertisement = ?`;
+
+  const descriptionQuery = `
+  SELECT
+	description.id,
+    advertisement,
+    updated,
+    value
+  FROM description
+  LEFT JOIN description_text
+  ON description_text.id = description.description_text_id
+  WHERE advertisement = ?`;
+
+  const customTagsQuery = `
+    SELECT * FROM olxdb.custom_tag
+    WHERE advertisement = ?`;
+
+	const id = req.query.id;
+
+  var responseData = {};
+
+  res.locals.mySQLDatabase.query(detailsQuery, [id])
+  .then(results => {
+    responseData.advertisementDetails = results;
+    return res.locals.mySQLDatabase.query(descriptionQuery, [id]);
+  }).then(results => {
+    responseData.advertisementDescription = results;
+    return res.locals.mySQLDatabase.query(customTagsQuery, [id]);
+  }).then(results => {
+    responseData.advertisementTags = results;
+    res.send({
+        "status": 200,
+        "error": null,
+        "details": responseData});
+  }).catch (error => {
+    res.send({"status": 500, "error": error});
+  });
+
+});
+
+module.exports = router;
