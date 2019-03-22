330a1b17d16a8d2e6d801745d39ec2ecf7b78fbd
Pavlo M <pavlomorozov78@gmail.com>
2019-03-22 11:10:13 +0200

    keyStatisitcsChart update

--- client/src/components/KeyStatistics/KeyStatisticsChart.js
+++ client/src/components/KeyStatistics/KeyStatisticsChart.js
@@ -12,22 +12,22 @@ class KeyStatisticsChart extends Component {
 
   render() {
     return (
-      <div className = "keyStatisitcsChart" style={{'border':'1px solid #4E648E'}}>
-
-      <ResponsiveContainer height={400}>
-
-        <LineChart data={this.state.chartData}>
-          <Line type="monotone" dataKey="opened" stroke="#2D882D" />
-          <Line type="monotone" dataKey="closed" stroke="#A6A938" />
-          <CartesianGrid stroke="#4E648E" strokeDasharray="10 10" />
-          <XAxis dataKey="timeInterval" stroke="#4E648E"/>
-          <YAxis stroke="#4E648E"/>
-          <Tooltip />
-        </LineChart>
-
-      </ResponsiveContainer>
-
-      < /div >
+      <div className = "keyStatisitcsChart">
+        <h4> Advertisements number posted
+        by user with 1 advertisement </h4>
+        <div style={{'border':'1px solid #4E648E'}}>
+          <ResponsiveContainer height={400}>
+            <LineChart data={this.state.chartData}>
+              <Line type="monotone" dataKey="opened" stroke="#2D882D" />
+              <Line type="monotone" dataKey="closed" stroke="#A6A938" />
+              <CartesianGrid stroke="#4E648E" strokeDasharray="10 10" />
+              <XAxis dataKey="timeInterval" stroke="#4E648E"/>
+              <YAxis stroke="#4E648E"/>
+              <Tooltip />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
       );
     }
   }
